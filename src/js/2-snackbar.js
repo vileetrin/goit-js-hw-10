1cd5506198bfa5679f306a351428f8f582c49ab8
Violetta Velykotrav <violettavelykotrav@gmail.com>
2024-03-23 00:47:28 +0200

    do task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = event.currentTarget.elements.delay;
+  const delay = parseInt(delayInput.value);
+  const state = event.currentTarget.elements.state.value;
+
+  const notificationPromise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+
+  notificationPromise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
